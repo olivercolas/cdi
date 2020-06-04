ff607f7d000a396e2f8c456399e61bcb7c32a941
olivercolas <olivercolas.oc@gmail.com>
2020-06-04 13:37:34 +0100

    refactor: reorganized to reflect new folder structure

---
+++ bin/index.js
@@ -0,0 +1,12 @@
+const clearConsole = require('../utils/clearConsole');
+const startMessage = require('../utils/startMessage');
+const { log } = console;
+
+const run = async () => {
+  clearConsole();
+  log(startMessage);
+
+  // run ls and get the current directory files
+};
+
+run();
