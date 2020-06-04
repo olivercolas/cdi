c30e9cdab334ba3cef05052c82cd5c5c5aafd095
olivercolas <olivercolas.oc@gmail.com>
2020-06-04 13:36:35 +0100

    feat: added clear console from facebook and a start message

---
+++ utils/startMessage.js
@@ -0,0 +1,6 @@
+const ANSI = require('ansi-art');
+
+module.exports = ANSI.get({
+  artName: 'mario',
+  speechText: `Welcome to CDI, the enhanced change directory command! Enjoy the ride and ctrl + c when you're at your destination!`,
+});
